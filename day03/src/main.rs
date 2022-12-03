ef2572a5b7cf9a8ac9c28d48568be79a71701338
Marc van Heerden <11750833+marcvanheerden@users.noreply.github.com>
2022-12-03 20:46:25 +0200

    clippy changes day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,10 +1,11 @@
-use std::fs;
 use itertools::Itertools;
+use std::fs;
 
 const UPPERCASE_START: u8 = 96;
 const LOWERCASE_START: u8 = 64;
 const ALPHABET_LEN: u32 = 26;
 const SEG_SIZE: usize = 3;
+const MAX_ASCII: usize = 123;
 
 fn main() {
     let input = fs::read_to_string("input10000").unwrap();
@@ -22,7 +23,7 @@ fn prior(item: char) -> u32 {
 }
 
 fn solution(contents: &str) -> (u32, u32) {
-    let mut buffer = [[false; 123]; SEG_SIZE];
+    let mut buffer = [[false; MAX_ASCII]; SEG_SIZE];
     let mut total_pt1 = 0u32;
     let mut total_pt2 = 0u32;
 
@@ -33,11 +34,9 @@ fn solution(contents: &str) -> (u32, u32) {
             for (idx, chr) in line.chars().enumerate() {
                 if idx < div {
                     buffer[0][chr as usize] = true;
-                } else {
-                    if buffer[0][chr as usize] {
-                        total_pt1 += prior(chr);
-                        break
-                    }
+                } else if buffer[0][chr as usize] {
+                    total_pt1 += prior(chr);
+                    break;
                 }
             }
             buffer[0] = [false; 123];
@@ -51,12 +50,12 @@ fn solution(contents: &str) -> (u32, u32) {
                 for chr in line.chars() {
                     if buffer[1][chr as usize] & buffer[2][chr as usize] {
                         total_pt2 += prior(chr);
-                        break
+                        break;
                     }
                 }
             }
         }
-        buffer = [[false; 123]; SEG_SIZE];
+        buffer = [[false; MAX_ASCII]; SEG_SIZE];
     }
     (total_pt1, total_pt2)
 }
