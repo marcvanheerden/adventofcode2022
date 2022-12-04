0857254fb99b4d7723c5e25464a0c13f6bb3b349
Marc van Heerden <11750833+marcvanheerden@users.noreply.github.com>
2022-12-04 07:26:55 +0200

    faster day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,8 @@
 use itertools::Itertools;
+use std::cmp::min;
 use std::fs;
+use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+use std::thread;
 
 const UPPERCASE_START: u8 = 96;
 const LOWERCASE_START: u8 = 64;
@@ -9,7 +12,7 @@ const MAX_ASCII: usize = 123;
 
 fn main() {
     let input = fs::read_to_string("input10000").unwrap();
-    let answer = solution(&input);
+    let answer = par_solution(&input, 10);
     println!("Part1: {} \nPart2: {}", answer.0, answer.1);
 }
 
@@ -22,12 +25,37 @@ fn prior(item: char) -> u32 {
     ((ascii - LOWERCASE_START) as u32) + ALPHABET_LEN
 }
 
-fn solution(contents: &str) -> (u32, u32) {
+fn par_solution(contents: &str, threads: usize) -> (u32, u32) {
+    let contents: Vec<&str> = contents.lines().collect();
+    let len = contents.len();
+    let mut step = len / threads;
+
+    // ensure each thread gets a multiple of the segment size
+    step = (step / SEG_SIZE + 1) * SEG_SIZE;
+
+    let atomic_output = (AtomicU32::new(0), AtomicU32::new(0));
+
+    thread::scope(|s| {
+        for thread in 0..threads {
+            let end = min((thread + 1) * step, len);
+            let slice = &contents[(thread * step)..end];
+            s.spawn(|| {
+                let output = solution(slice);
+                atomic_output.0.fetch_add(output.0, Relaxed);
+                atomic_output.1.fetch_add(output.1, Relaxed);
+            });
+        }
+    });
+
+    (atomic_output.0.into_inner(), atomic_output.1.into_inner())
+}
+
+fn solution(contents: &[&str]) -> (u32, u32) {
     let mut buffer = [[false; MAX_ASCII]; SEG_SIZE];
     let mut total_pt1 = 0u32;
     let mut total_pt2 = 0u32;
 
-    for chunk in &contents.lines().chunks(SEG_SIZE) {
+    for chunk in &contents.iter().chunks(SEG_SIZE) {
         for (idx, line) in chunk.enumerate() {
             // part 1
             let div = line.len() / 2;
@@ -55,7 +83,8 @@ fn solution(contents: &str) -> (u32, u32) {
                 }
             }
         }
-        buffer = [[false; MAX_ASCII]; SEG_SIZE];
+        buffer[1] = [false; 123];
+        buffer[2] = [false; 123];
     }
     (total_pt1, total_pt2)
 }
@@ -79,6 +108,11 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[test]
     fn example() {
-        assert_eq!(solution(INPUT), (157, 70));
+        let input: Vec<&str> = INPUT.lines().collect();
+        assert_eq!(solution(&input), (157, 70));
+    }
+    #[test]
+    fn par_example() {
+        assert_eq!(par_solution(INPUT, 1), (157, 70));
     }
 }
